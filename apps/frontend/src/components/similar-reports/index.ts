44cf8e47157f04c124f46b82e3e0e8e72330f09e
jaky <lapekobos@gmail.com>
2026-08-29 14:11:36 +0700

    JEK -39, (fe) : [Frontend Lapor] Tampilan deteksi laporan serupa

---
+++ apps/frontend/src/components/similar-reports/index.ts
@@ -0,0 +1,2 @@
+export { default as SimilarReportsSuggestion } from './SimilarReportsSuggestion'
+export type { ReportSimilarItem, SimilarReportsParams, SimilarReportsSuggestionProps } from './types'
